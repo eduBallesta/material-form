7033fd14928379fe4a778104fda13a66a30ca5af
Edu Ballesta <grupokone@gmail.com>
2023-08-31 23:23:16 +0200

    Delete self directory

--- self/src/app/model/tax-payer.model.ts
+++
@@ -1,5 +0,0 @@
-export class TaxPayer {
-    constructor(public name: string, public firstName: string, public address: string,
-        public phoneNumber: string, public email: string) {
-    }
- }
